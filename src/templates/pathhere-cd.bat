83d9f0b26669a5c047227dc8f09d82c7c172fbe4
Cologler <skyoflw@gmail.com>
2018-08-09 13:01:12 +0800

    update templates

--- src/templates/pathhere-cd.bat
+++ src/templates/pathhere-cd.bat
@@ -1,9 +1,13 @@
 @REM this script was created by scripthis
-@ECHO off
-IF DEFINED PATH_HERE (
-    SET PATH_HERE="{path}";%PATH_HERE%
-) ELSE (
-    SET PATH_HERE="{path}"
+@echo off
+if defined PATH_HERE (
+    set PATH_HERE="{path}";%PATH_HERE%
+) else (
+    set PATH_HERE="{path}"
 )
-CD "{path}"
-ECHO cd {path}
+if exist "{path}" (
+    cd "{path}"
+    echo chdir: {path}
+) else (
+    echo {path} does not exists, please reset the bat file using scripthis.
+)
--- src/templates/pathhere.bat
+++ src/templates/pathhere.bat
@@ -1,9 +1,13 @@
 @REM this script was created by scripthis
-@ECHO off
-IF DEFINED PATH_HERE (
-    SET PATH_HERE="{path}";%PATH_HERE%
-) ELSE (
-    SET PATH_HERE="{path}"
+@echo off
+if defined PATH_HERE (
+    set PATH_HERE="{path}";%PATH_HERE%
+) else (
+    set PATH_HERE="{path}"
 )
-PATH = "{path}";%PATH%
-ECHO added {path} to path.
+if exist "{path}" (
+    set PATH="{path}";%PATH%
+    echo added {path} to path.
+) else (
+    echo {path} does not exists, please reset the bat file using scripthis.
+)
