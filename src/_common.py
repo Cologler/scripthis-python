29cb39d61bff756269bc8715f2f34afc961a0298
Cologler <skyoflw@gmail.com>
2019-04-13 18:18:11 +0800

    refactor

--- src/_common.py
+++ src/_common.py
@@ -9,6 +9,7 @@ import os
 import sys
 import logging
 
+import click
 import colorama
 import coloredlogs
 from fsoopify import Path, FileInfo
@@ -38,52 +39,55 @@ if not os.path.isdir(SCRIPTS_ROOT):
     exit(2)
 
 class BaseApp:
+    ctx: click.Context = None
+    path: str = None
+    name: str = None
+    env: str = None
+    rel: str = None
+
     def __init__(self):
-        self._name = MAIN_NAME
-        self._logger = APP_LOGGER
+        self._logger = get_logger(MAIN_NAME)
         self._scripts_root = SCRIPTS_ROOT
 
-    def run(self, argv):
-        raise NotImplementedError
-
-    def log_info(self, msg: str, *args):
-        def var(text): # pylint: disable=C0111
+    def info(self, msg: str, *args):
+        def wrap(text): # pylint: disable=C0111
             return f'{colorama.Fore.LIGHTGREEN_EX}{text}{colorama.Fore.RESET}'
-
-        msg = msg.format(*[var(t) for t in args])
+        msg = msg.format(*[wrap(t) for t in args])
         self._logger.info(msg)
 
-    def log_error(self, msg: str, *args):
-        def var(text): # pylint: disable=C0111
+    def error(self, msg: str, *args):
+        def wrap(text): # pylint: disable=C0111
+            return click.style(text, fg='green')
             return f'{colorama.Fore.LIGHTGREEN_EX}{text}{colorama.Fore.RESET}'
-
-        msg = msg.format(*[var(t) for t in args])
-        self._logger.error(msg)
+        msg = msg.format(*[wrap(t) for t in args])
+        return self.ctx.fail(msg)
 
     def load_template(self, name):
         path = os.path.join(Path(sys.argv[0]).dirname, 'templates', name)
         return FileInfo(path).read_text()
 
     def write_script(self, path: Path, dest_name: str, content: str):
-        self.log_info('Creating {} for {}', dest_name + '.bat', path)
+        self.info('Creating {} for {}', dest_name + '.bat', path)
 
         dest_path = os.path.join(self._scripts_root, dest_name + '.bat')
 
         if os.path.isfile(dest_path):
-            self.log_info('Overwriting exists file: {}', dest_path)
+            self.info('Overwriting exists file: {}', dest_path)
 
         with open(dest_path, 'w', encoding='utf8') as fp:
             fp.write(content)
 
-        self.log_info('Done.')
+        self.info('Done.')
 
+def make_rel_path(path: str, base_path, replacement: str):
+    index = os.path.normcase(path).find(os.path.normcase(base_path))
+    if index < 0:
+        return path
+    return path[0:index] + replacement + path[index+len(base_path):]
 
-def path_rel_onedrive(path: str):
+def make_rel_path_for_onedrive(path: str):
     onedrive_path = os.environ.get('OneDrive', None)
     if onedrive_path is None:
         APP_LOGGER.error('OneDrive path not detected.')
         exit(1)
-    index = os.path.normcase(path).find(os.path.normcase(onedrive_path))
-    if index < 0:
-        return path
-    return path[0:index] + r'%onedrive%' + path[index+len(onedrive_path):]
+    return make_rel_path(path, onedrive_path, r'%onedrive%')
--- src/scripthis.py
+++ src/scripthis.py
@@ -20,14 +20,70 @@ import traceback
 
 from docopt import docopt
 import colorama
-from fsoopify import Path, FileInfo
+from fsoopify import Path, FileInfo, NodeInfo, NodeType
+import click
+from click_anno import command
 
-from _common import BaseApp, path_rel_onedrive
+from _common import BaseApp, make_rel_path_for_onedrive
 
-KEY_EXEC = 'exec'
-KEY_INIT = 'init'
-KEY_TEMPLATE = 'template'
-KEY_EXT_TABLE = 'ext-table'
+def list_executeable_exts():
+    pathext = os.getenv('PATHEXT').lower()
+    return pathext.split(';') + list(EXECUTOR_INFO_TABLE.keys())
+
+class App(BaseApp):
+
+    def find_abspath(self):
+        path = os.path.abspath(self.path)
+        if os.path.isfile(path) or os.path.isdir(path):
+            return path
+        for ext in list_executeable_exts():
+            realpath = path + ext
+            if os.path.isfile(realpath):
+                return realpath
+
+        styled_path = click.style(path, fg='green')
+        self.error('{} does not exists', path)
+
+    def get_rel_func(self):
+        if self.rel is None:
+            return lambda x: x
+        elif self.rel.lower() == 'onedrive':
+            return make_rel_path_for_onedrive
+        else:
+            self.error('unknown rel args: {}', self.rel)
+
+    def get_executor_info(self, node: FileInfo):
+        root_key = node.path.name.ext.lower() if node.node_type == NodeType.file else 'folder'
+        executor_info = EXECUTOR_INFO_TABLE.get(root_key)
+
+        if executor_info and self.env:
+            executor_info = executor_info.envs.get(self.env.lower())
+
+        if executor_info:
+            return executor_info
+        elif self.env:
+            self.error('unknown env: {}', self.env)
+        else:
+            return ExecutorInfo()
+
+    def __call__(self):
+        env = self.env
+        arg_name = self.name
+        rel_func = self.get_rel_func()
+
+        node = NodeInfo.from_path(self.find_abspath())
+        executor_info = self.get_executor_info(node)
+
+        args = {
+            'path': node.path,
+            'dir': os.path.dirname(node.path)
+        }
+        executor_info.update_args(self, args)
+        for k, v in list(args.items()):
+            args[k] = rel_func(v)
+
+        content = self.load_template(executor_info.get_template()).format_map(args)
+        self.write_script(args['path'], arg_name or node.path.pure_name, content)
 
 class ExecutorInfo:
     def __init__(self, executor='', **envs):
@@ -37,7 +93,7 @@ class ExecutorInfo:
     def get_template(self):
         return 'scripthis.bat'
 
-    def update_args(self, args: dict):
+    def update_args(self, app: App, args: dict):
         if self.executor:
             args['eval'] = self.executor + ' '
         else:
@@ -47,8 +103,8 @@ class PipenvExecutorInfo(ExecutorInfo):
     def get_template(self):
         return 'scripthis-pipenv.bat'
 
-    def update_args(self, args: dict):
-        super().update_args(args)
+    def update_args(self, app: App, args: dict):
+        super().update_args(app, args)
         script_file = FileInfo(args['path'])
         args['pipfile'] = self._find_pipfile_path(script_file, 5)
 
@@ -62,8 +118,13 @@ class PipenvExecutorInfo(ExecutorInfo):
             depth -= 1
         raise RuntimeError('cannot found Pipfile.')
 
+class FolderExecutorInfo(ExecutorInfo):
+    def update_args(self, app: App, args: dict):
+        app.error('folder without env is not callable')
+
 
 EXECUTOR_INFO_TABLE = {
+    'folder': FolderExecutorInfo(),
     '.jar': ExecutorInfo(executor='java -jar'),
     '.js': ExecutorInfo(executor='node'),
     '.ts': ExecutorInfo(executor='ts-node'),
@@ -71,77 +132,29 @@ EXECUTOR_INFO_TABLE = {
         pipenv=PipenvExecutorInfo(executor='pipenv run python')
     )
 }
+EXECUTOR_INFO_TABLE['folder'].envs['py-m'] = ExecutorInfo(executor='python -m')
+EXECUTOR_INFO_TABLE['folder'].envs['python-m'] = ExecutorInfo(executor='python -m')
+EXECUTOR_INFO_TABLE['folder'].envs['pipenv-py-m'] = PipenvExecutorInfo(executor='pipenv run python -m')
+EXECUTOR_INFO_TABLE['folder'].envs['pipenv-python-m'] = PipenvExecutorInfo(executor='pipenv run python -m')
 
-class App(BaseApp):
-    def error_unknown_env(self, env):
-        self.log_error('unknown env: {}', env)
-        return exit(1)
-
-    def run(self, argv):
-        if not argv:
-            msg = 'Please provide a executable file path.'
-            msg += '\n' + colorama.Fore.RESET
-            msg += ' ' * 19 + 'For example: `scripthis FILE.exe`'
-            self.log_error(msg)
-            exit(3)
-
-        args = docopt(__doc__)
-        source = args['<FILE>']
-        env = args['--env']
-        rel = args['--rel']
-        arg_name = args['--name']
-
-        def list_executeable_exts():
-            pathext = os.getenv('PATHEXT').lower()
-            return pathext.split(';') + list(EXECUTOR_INFO_TABLE.keys())
-
-        def try_detect_path():
-            p = os.path.abspath(source)
-            if os.path.isfile(p):
-                return p
-            for ext in list_executeable_exts():
-                if os.path.isfile(p + ext):
-                    return p + ext
-            self.log_error('{} is not a file', p)
-            exit()
-
-        path = Path(try_detect_path())
-
-        executor_info = EXECUTOR_INFO_TABLE.get(path.name.ext.lower())
-        if executor_info is not None and env:
-            executor_info = executor_info.envs.get(env.lower())
-
-        if not executor_info and env:
-            return self.error_unknown_env(env)
-
-        executor_info = executor_info or ExecutorInfo()
-
-        if rel is None:
-            rel_co = lambda x: x
-        elif rel.lower() == 'onedrive':
-            rel_co = path_rel_onedrive
-        else:
-            print(f'unknown rel args: {rel}')
-            return exit(1)
-
-        args_table = {
-            'path': path,
-            'dir': os.path.dirname(path)
-        }
-        executor_info.update_args(args_table)
-        for k, v in list(args_table.items()):
-            args_table[k] = rel_co(v)
-
-        content = self.load_template(executor_info.get_template()).format_map(args_table)
-        self.write_script(path, arg_name or path.pure_name, content)
+@command
+def cli(ctx: click.Context, path: str, name: str=None, env: str=None, rel: str=None):
+    app = App()
+    app.__dict__.update({
+        'ctx': ctx,
+        'path': path,
+        'name': name,
+        'env': env,
+        'rel': rel,
+    })
+    app()
 
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
     try:
-        app = App()
-        app.run(argv[1:])
+        cli()
     except Exception:
         traceback.print_exc()
         input()
